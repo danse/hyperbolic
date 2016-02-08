c1b7f47dd96fef3291eae9654ca6387ce842bc11
danse <f.occhipinti@gmail.com>
2016-02-08 16:13:13 +0100

    added two functions to calculate rates for intervals

--- Main.hs
+++ Main.hs
@@ -1,6 +1,8 @@
-import Text.PrettyPrint.Boxes
+import Text.PrettyPrint.Boxes hiding ((*))
 
 import Hyperbola
+
+data Interval = Interval { firstHour :: Int, lastHour :: Int } deriving Show
 
 average x = s/n
   where n = fromIntegral $ length x
@@ -18,20 +20,42 @@ are interested in a small number of iterations though, corresponding
 to the number of hours which fit in a working week
 
 -}
-harmonicSum first last
-  | first > last = 0
-  | otherwise    = 1/first + others
-  where others = harmonicSum (first+1) last
+harmonicSum :: Interval -> Float
+harmonicSum (Interval { firstHour=f, lastHour=l })
+  | f > l  = 0
+  | otherwise = (1/(fromIntegral f)) + others
+  where others = harmonicSum (Interval { firstHour=f+1, lastHour=l })
 
-hoursPerWeek = 35
+multiplierFromPayedInterval :: Float -> Interval -> Float
+multiplierFromPayedInterval rate int = totalPaid/totalWeight
+  where totalWeight = harmonicSum int
+        totalPaid = rate * ((fromIntegral $ firstHour int) - (fromIntegral $ lastHour int))
+
+-- build an interval with the given amount of hours, allocating them
+-- in the middle of the week in order to free both cheaper and more
+-- expensive hours. Prefer allocating cheaper hours if uneven
+--
+-- >>> centeredInterval 34
+-- Interval { firstHour: 1, lastHour: 34 }
+--
+-- >>> centeredInterval 6
+-- Interval { firstHour: 14, lastHour: 20 }
+centeredInterval :: Int -> Interval
+centeredInterval hours = Interval { firstHour=f, lastHour=l }
+  where t = quot (hoursPerWeek - hours) 2
+        f = t
+        l = t + hours
+
+hoursPerWeek = 35 :: Int
 
 cheapest hour = 41 - hour
 
-priceLine mul hour = (b employedHours) <+> (b hourPrice) <+> (b monthPrice)
-  where employedHours = cheapest hour
-        hourPrice = hyperbola mul hour
-        monthPrice = 4 * hoursPerWeek * hourPrice
-        b = text . show
+priceLine :: Float -> Int -> Box
+priceLine mul hour = employedHours <+> hourPriceBox <+> monthPrice
+  where employedHours = text $ show $ cheapest hour
+        hourPrice = hyperbola mul (fromIntegral hour)
+        hourPriceBox = text $ show $ hourPrice
+        monthPrice = text $ show $ 4 * (fromIntegral hoursPerWeek) * hourPrice
 
 priceTable mul = vcat left $ map (priceLine mul) [1..hoursPerWeek]
 
