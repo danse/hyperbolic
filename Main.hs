26210ecbaddc92e616c48257717189ff3cd8656a
danse <f.occhipinti@gmail.com>
2016-02-08 12:49:16 +0100

    start calculations about intervals

--- Main.hs
+++ Main.hs
@@ -6,6 +6,23 @@ average x = s/n
   where n = fromIntegral $ length x
         s = sum x
 
+{-
+
+Let's say that i want to kwon the cumulative rate of hours between the
+second and the sixt:
+
+    r = k/2 + k/3 + k/4 + k/5 + k/6 = k (1/2 + 1/3 + 1/4 + 1/5 + 1/6)
+
+This is related to the harmonic series, which is divergent. Here we
+are interested in a small number of iterations though, corresponding
+to the number of hours which fit in a working week
+
+-}
+coefficientMultiplier first last
+  | first > last = 0
+  | otherwise    = 1/first + others
+  where others = coefficientMultiplier (first+1) last
+
 hoursPerWeek = 40
 
 cheapest hour = 41 - hour
