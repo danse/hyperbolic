73baaf83130279bd502278858a932e058e5d571e
danse <f.occhipinti@gmail.com>
2016-02-22 18:48:38 +0100

    specify better what is the personal time for

--- Conf.hs
+++ Conf.hs
@@ -4,6 +4,7 @@ daily = 7
 perWeek = 5 * daily :: Int
 
 total = perWeek -- * 4 for months instead of weeks
--- personal development: time for studying etcetera
+-- personal development: time for studying, free software, logistics,
+-- connections etcetera
 personal = round (0.3 * (fromIntegral total))
 target = total - personal
