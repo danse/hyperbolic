17e797ce347d5522926b638832c58b204da1243b
danse <f.occhipinti@gmail.com>
2017-03-21 03:09:48 +0100

    reduce the amount of reserved hours

--- Conf.hs
+++ Conf.hs
@@ -4,7 +4,10 @@ daily = 7
 perWeek = 5 * daily :: Int
 
 total = perWeek -- * 4 for months instead of weeks
+
 -- personal development: time for studying, free software, logistics,
--- connections etcetera
-personal = round (0.3 * (fromIntegral total))
+-- connections etcetera. This is set rather low, for now, because the
+-- algorithm in itself is aimed at protecting hours useful for
+-- personal development
+personal = round (0.1 * (fromIntegral total))
 target = total - personal
